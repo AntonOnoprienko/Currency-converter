b432330d8e663f29eea29e16777e1373eda7b394
AntonOnoprienko <onoprienkoanton88@gmail.com>
2022-06-24 23:42:57 +0300

    Complited version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core'
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css'],
+	
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+	
+	ngOnInit(): void {
+		this.getCurrencies();
+	}
+	date = new Date().toDateString();
+	rates = {
+		USD: 0,
+		EUR: 0,
+		UAH: 1
+	}
+	async getCurrencies() {
+		const response = await fetch('https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json');
+		const data = await response.json()
+		const result = await data
+
+		function getCurrencyRate(rates: any[], cc: string) {
+			return rates.find(r => r.cc === cc).rate.toFixed(3)
+		}
+
+		this.rates.USD = getCurrencyRate(result, 'USD')
+		this.rates.EUR = getCurrencyRate(result, 'EUR')
+	}
+
+	input: any
+	output: any
+
+	getValueOutput(x:any,y:any,z:any) {
+		const value = x * y / z 
+		this.output = value.toFixed(2)
+	}
+
+	getValueInput(x:any,y:any,z:any) {
+		const value = x * y / z 
+		this.input = value.toFixed(2)
+	}
+
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component'
 
-import { AppComponent } from './app.component';
-import { CardComponent } from "./card/card.component";
-import { FormComponent } from './form/form.component';
 
 @NgModule({
   declarations: [
 		AppComponent,
-	  CardComponent,
-   FormComponent
   ],
-  imports: [
-    BrowserModule
+	imports: [
+		BrowserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
