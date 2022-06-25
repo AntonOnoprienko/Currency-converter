f6e99c0918c233ba91c264e9cf971eb7a6f8d277
AntonOnoprienko <onoprienkoanton88@gmail.com>
2022-06-25 21:33:00 +0300

    refactoring

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,40 +6,45 @@ import { Component, OnInit} from '@angular/core'
 	styleUrls: ['./app.component.css'],
 	
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
 	
 	ngOnInit(): void {
 		this.getCurrencies();
 	}
-	date = new Date().toDateString();
-	rates = {
-		USD: 0,
-		EUR: 0,
-		UAH: 1
-	}
-	async getCurrencies() {
-		const response = await fetch('https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json');
-		const data = await response.json()
-		const result = await data
 
-		function getCurrencyRate(rates: any[], cc: string) {
-			return rates.find(r => r.cc === cc).rate.toFixed(3)
-		}
+	date = new Date();
 
-		this.rates.USD = getCurrencyRate(result, 'USD')
-		this.rates.EUR = getCurrencyRate(result, 'EUR')
-	}
+	rates = [
+		{ current: "USD", value: 0 },
+		{ current: "EUR", value: 0 },
+		{ current: "UAH", value: 1 }
+	];
 
 	input: any
 	output: any
 
-	getValueOutput(x:any,y:any,z:any) {
-		const value = x * y / z 
+
+	async getCurrencies() {
+		const response = await fetch('https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json');
+		const data = await response.json()
+		const result = await data
+		
+		function getCurrencyRate(rates: any[], cc: string) {
+			return rates.find(r => r.cc === cc).rate
+		}
+
+		this.rates[0].value = getCurrencyRate(result, 'USD')
+		this.rates[1].value = getCurrencyRate(result, 'EUR')
+	}
+
+
+	getOutputValue(input: any, leftselect: any, rightselect: any) {
+		const value = input * leftselect / rightselect 
 		this.output = value.toFixed(2)
 	}
 
-	getValueInput(x:any,y:any,z:any) {
-		const value = x * y / z 
+	getInptValue(output: any, rightselect: any, leftselect: any) {
+		const value = output * rightselect / leftselect 
 		this.input = value.toFixed(2)
 	}
 
